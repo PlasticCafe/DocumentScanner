fe588ac8c6a98412a177c5c58070aa361ce6caec
PlasticCafe <jdiaz0612@gmail.com>
2019-01-11 07:51:57 -0600

    Change drawables to vector format.

--- app/src/main/java/cafe/plastic/documentscanner/ui/Bindings.java
+++ app/src/main/java/cafe/plastic/documentscanner/ui/Bindings.java
@@ -12,22 +12,11 @@ import io.fotoapparat.selector.FlashSelectorsKt;
 import kotlin.jvm.functions.Function1;
 
 public class Bindings {
-    @BindingAdapter({"flashState", "flashOff", "flashOn", "flashAuto"})
-    public static void updateFlashButton(ImageButton view, CameraState.Flash flashState, Drawable flashOff, Drawable flashOn, Drawable flashAuto) {
-        Drawable drawable;
-        switch (flashState) {
-            case ON:
-                drawable = flashOn;
-                break;
-            case OFF:
-                drawable = flashOff;
-                break;
-            case AUTO:
-                drawable = flashAuto;
-                break;
-            default:
-                drawable = flashOff;
-        }
-        view.setImageDrawable(drawable);
+    @BindingAdapter({"flashState", "flashOff", "flashOn"})
+    public static void updateFlashButton(ImageButton view, CameraState.Flash flashState, Drawable flashOff, Drawable flashOn) {
+        if(flashState == CameraState.Flash.ON)
+            view.setImageDrawable(flashOn);
+        else
+            view.setImageDrawable(flashOff);
     }
 }
